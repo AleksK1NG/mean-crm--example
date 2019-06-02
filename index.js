ee4664a75b186482af49b8ac35f777b8bb27bc94
Alexander Bryksin <alexander.bryksin@yandex.ru>
2019-06-02 18:40:14 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const app = express();
+
+// Body Parser
+app.use(express.json());
+
+// PORT
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server listen on port: ${port} `));
