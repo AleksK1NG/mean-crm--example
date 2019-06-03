e26617c630672daa6916cfde55df62daff90e570
Alexander Bryksin <alexander.bryksin@yandex.ru>
2019-06-03 12:46:29 +0300

    add config mongo mlab

--- db.js
+++ db.js
@@ -2,6 +2,8 @@ const mongoose = require('mongoose');
 const config = require('config');
 const db = config.get('mongoURI');
 
+// const db = 'mongodb://Alex:Alexpro777@ds217976.mlab.com:17976/mean-crm';
+
 const connectDB = async () => {
   try {
     await mongoose.connect(db, { useNewUrlParser: true, useCreateIndex: true, useFindAndModify: false });
