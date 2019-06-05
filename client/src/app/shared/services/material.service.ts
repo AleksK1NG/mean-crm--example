f544441fc857cc5e83580ae54f0bc8aafa2345a4
Alexander Bryksin <alexander.bryksin@yandex.ru>
2019-06-05 12:06:48 +0300

    add Client MaterialService toast

---
+++ client/src/app/shared/services/material.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+
+declare var M;
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MaterialService {
+  constructor() {}
+
+  static toast(message: string) {
+    M.toast({ html: message, classes: 'rounded' });
+  }
+}
