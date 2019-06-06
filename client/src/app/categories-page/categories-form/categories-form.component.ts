5ff87b1de79f410dabefd3fb0ccf9556e815bf5b
Alexander Bryksin <alexander.bryksin@yandex.ru>
2019-06-06 18:39:05 +0300

    add Client CategoryService Subject CRUD

--- client/src/app/categories-page/categories-form/categories-form.component.ts
+++ client/src/app/categories-page/categories-form/categories-form.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { CategoriesService } from '../../shared/services/categories.service';
+import { Category } from '../../shared/interfaces/category';
 
 @Component({
   selector: 'app-categories-form',
@@ -7,16 +10,51 @@ import { ActivatedRoute, Params } from '@angular/router';
   styleUrls: ['./categories-form.component.css']
 })
 export class CategoriesFormComponent implements OnInit {
-  constructor(private route: ActivatedRoute) {}
+  constructor(private route: ActivatedRoute, private categoriesService: CategoriesService) {}
   private id: string;
   private isNew = true;
 
+  private form: FormGroup;
+
   ngOnInit() {
+    this.form = new FormGroup({
+      name: new FormControl(null, Validators.required)
+    });
     this.route.params.subscribe((params: Params) => {
       if (params['id']) {
         this.isNew = false;
+        this.id = params['id'];
+        console.log(params.id);
+        console.log('id => ', this.id);
       }
     });
-    console.log(this.id);
+  }
+
+  onSubmit() {
+    console.log('Add Category => ', this.form.value);
+
+    this.categoriesService.addCategory(this.form.value).subscribe(
+      (categories: Category[]) => {
+        console.log('Categories after Add Category call => ', categories);
+      },
+      (error) => {
+        console.error('Categories ERROR after Add Category call => ', error);
+      }
+    );
+  }
+
+  deleteCategory() {
+    console.log('Delete Category ID from state => ', this.id);
+    if (this.id) {
+      this.categoriesService.deleteCategory(this.id).subscribe(
+        (categories: Category[]) => {
+          console.log('Categories after Add Category call => ', categories);
+        },
+        (error) => {
+          console.error('Categories ERROR after Add Category call => ', error);
+        }
+      );
+    }
   }
 }
+
--- client/src/app/shared/services/categories.service.ts
+++ client/src/app/shared/services/categories.service.ts
@@ -2,31 +2,30 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Category } from '../interfaces/category';
 import { BehaviorSubject, Observable, Subject } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CategoriesService {
-  constructor(private httpClient: HttpClient) {}
+  constructor(private httpClient: HttpClient, private router: Router) {}
 
-  // private error = null;
   private errorSub$ = new BehaviorSubject<any>(null);
   private categories: Category[] = [];
+  private currentCategory: Category = null;
+
   public isLoading$ = new BehaviorSubject<boolean>(false);
-
   private categoriesSub$ = new Subject<Category[]>();
+  private categoriesList$ = new BehaviorSubject<Category[]>(this.categories);
+  private currentCategorySub$ = new BehaviorSubject<Category>(null);
 
-  // fetchAllCategories(): Observable<Category[]> {
-  //   console.log('fetch all categories');
-  //   this.isLoading$.next(true);
-  //   return this.httpClient.get<Category[]>('/api/v1/category');
-  // }
 
   getAllCategories(): Subject<Category[]> {
     // const categoriesSub$ = new Subject<Category[]>();
     this.isLoading$.next(true);
     this.httpClient.get<Category[]>('/api/v1/category').subscribe(
       (categories: Category[]) => {
+        this.categoriesList$.next(categories);
         this.categoriesSub$.next(categories);
         this.errorSub$.next(null);
         this.isLoading$.next(false);
@@ -37,22 +36,93 @@ export class CategoriesService {
       }
     );
 
-    return this.categoriesSub$;
+    // return this.categoriesSub$;
+    return this.categoriesList$;
   }
 
-  // setError(error) {
-  //   console.error(error);
-  //   this.error = error;
-  //   this.isLoading$.next(false);
-  // }
-  //
-  // setCategories(categories: Category[]) {
-  //   this.categories = categories;
-  //   this.isLoading$.next(false);
-  // }
+  getCategory(categoryId: string): BehaviorSubject<Category> {
+    this.isLoading$.next(true);
 
-  // getCategories(): Category[] {
-  //   return this.categories;
-  // }
+    this.httpClient.get<Category>(`/api/v1/category/${categoryId}`).subscribe(
+      (category: Category) => {
+        this.currentCategorySub$.next(category);
+        this.errorSub$.next(null);
+        this.isLoading$.next(false);
+      },
+      (error) => {
+        this.errorSub$.next(error);
+        this.isLoading$.next(false);
+      }
+    );
+
+    return this.currentCategorySub$;
+  }
+
+  addCategory(newCategory: Category): BehaviorSubject<Category[]> {
+    this.isLoading$.next(true);
+    this.httpClient.post<Category>('/api/v1/category', newCategory).subscribe(
+      (category: Category) => {
+        this.categories.push(category);
+        this.categoriesList$.next(this.categories);
+        this.errorSub$.next(null);
+        this.isLoading$.next(false);
+        this.router.navigate(['/categories'])
+      },
+      (error) => {
+        this.errorSub$.next(error);
+        this.isLoading$.next(false);
+      }
+    );
+
+    // return this.categoriesSub$;
+    return this.categoriesList$;
+  }
+
+  updateCategory(newCategory: Category): BehaviorSubject<Category[]> {
+    this.isLoading$.next(true);
+    this.httpClient.patch<Category>(`/api/v1/category/${newCategory._id}`, newCategory).subscribe(
+      (updatedCategory: Category) => {
+        this.categories = this.categories.map((cat: Category) =>
+          cat._id === updatedCategory._id ? updatedCategory : cat
+        );
+
+        this.categoriesList$.next(this.categories);
+        this.errorSub$.next(null);
+        this.isLoading$.next(false);
+        this.router.navigate(['/categories'])
+      },
+      (error) => {
+        this.errorSub$.next(error);
+        this.isLoading$.next(false);
+      }
+    );
+
+    // return this.categoriesSub$;
+    return this.categoriesList$;
+  }
+
+  deleteCategory(categoryId: string): BehaviorSubject<Category[]> {
+    this.isLoading$.next(true);
+    this.httpClient.delete<Category>(`/api/v1/category/${categoryId}`).subscribe(
+      (deletedCategory: Category) => {
+        this.categories = this.categories.filter((cat: Category) => cat._id !== categoryId);
+
+        this.categoriesList$.next(this.categories);
+        this.errorSub$.next(null);
+        this.isLoading$.next(false);
+        this.router.navigate(['/categories'])
+      },
+      (error) => {
+        this.errorSub$.next(error);
+        this.isLoading$.next(false);
+      }
+    );
+
+    // return this.categoriesSub$;
+    return this.categoriesList$;
+  }
+
+
 }
+
 
