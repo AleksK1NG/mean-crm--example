aa8cf46ec4396ce6232e34627cc1f42893d01543
Alexander Bryksin <alexander.bryksin@yandex.ru>
2019-06-08 19:49:16 +0300

    add Client order-page modal

--- client/src/app/order-page/order-page.component.ts
+++ client/src/app/order-page/order-page.component.ts
@@ -1,15 +1,46 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+import { MaterialService } from '../shared/services/material.service';
+import { MaterialInstance } from '../shared/interfaces/materialInstance';
 
 @Component({
   selector: 'app-order-page',
   templateUrl: './order-page.component.html',
   styleUrls: ['./order-page.component.css']
 })
-export class OrderPageComponent implements OnInit {
+export class OrderPageComponent implements OnInit, OnDestroy, AfterViewInit {
+  private isRoot: boolean;
+  @ViewChild('modal', null) modalRef: ElementRef;
+  public modal: MaterialInstance;
 
-  constructor() { }
+  constructor(private router: Router) {}
 
   ngOnInit() {
+    this.isRoot = this.router.url === '/order';
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        this.isRoot = this.router.url === '/order';
+      }
+    });
   }
 
+  ngAfterViewInit() {
+    this.modal = MaterialService.initModal(this.modalRef);
+  }
+
+  ngOnDestroy() {
+    this.modal.destroy();
+  }
+
+  open() {
+    this.modal.open();
+  }
+
+  cancel() {
+    this.modal.close();
+  }
+
+  submit() {
+    this.modal.close();
+  }
 }
