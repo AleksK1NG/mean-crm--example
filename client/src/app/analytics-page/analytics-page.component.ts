ea96f29ed72ac4a2bb5b6a1b2838a5027c41f654
Alexander Bryksin <alexander.bryksin@yandex.ru>
2019-06-11 15:33:51 +0300

    add Client Analytics Charts

--- client/src/app/analytics-page/analytics-page.component.ts
+++ client/src/app/analytics-page/analytics-page.component.ts
@@ -27,16 +27,41 @@ export class AnalyticsPageComponent implements OnInit, AfterViewInit, OnDestroy 
       color: 'rgb(255, 99, 132)'
     };
 
+    const orderConfig: any = {
+      label: 'orders',
+      color: 'rgb(54, 162, 235)'
+    };
+
     this.averageSub$ = this.analyticsService.getAnalytics().subscribe((data: AnalyticsPage) => {
       this.average = data.average;
       console.log(data);
       gainConfig.labels = data.chart.map((item) => item.label);
       gainConfig.data = data.chart.map((item) => item.gain);
 
+      orderConfig.labels = data.chart.map((item) => item.label);
+      orderConfig.data = data.chart.map((item) => item.order);
+
+      // temp mock Gain Data
+      // gainConfig.labels.push('10.05.2019');
+      // gainConfig.labels.push('12.05.2019');
+      // gainConfig.data.push(1500);
+      // gainConfig.data.push(25700);
+      // temp mock Gain Data
+
+      // temp mock Orders Data
+      // orderConfig.labels.push('10.05.2019');
+      // orderConfig.labels.push('12.05.2019');
+      // orderConfig.data.push(1500);
+      // orderConfig.data.push(25700);
+      // temp mock Orders Data
+
       const gainCtx = this.gainRef.nativeElement.getContext('2d');
+      const orderCtx = this.orderRef.nativeElement.getContext('2d');
       gainCtx.canvas.height = '300px';
+      orderCtx.canvas.height = '300px';
 
       new Chart(gainCtx, createChartConfig(gainConfig));
+      new Chart(orderCtx, createChartConfig(orderConfig));
 
       this.loading = false;
     });
