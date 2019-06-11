5a682c8f75dd485209cce5e7ce40f72d2132122f
Alexander Bryksin <alexander.bryksin@yandex.ru>
2019-06-11 14:29:08 +0300

    add Client Analytics Page tapTarget info

--- client/src/app/overview-page/overview-page.component.ts
+++ client/src/app/overview-page/overview-page.component.ts
@@ -1,19 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { AnalyticsService } from '../shared/services/analytics.service';
 import { Observable } from 'rxjs';
 import { OverviewPage } from '../shared/interfaces/overviewPage';
+import { MaterialInstance } from '../shared/interfaces/materialInstance';
+import { MaterialService } from '../shared/services/material.service';
 
 @Component({
   selector: 'app-overview-page',
   templateUrl: './overview-page.component.html',
   styleUrls: ['./overview-page.component.css']
 })
-export class OverviewPageComponent implements OnInit {
+export class OverviewPageComponent implements OnInit, OnDestroy, AfterViewInit {
+  @ViewChild('tapTarget', null) tapTargetRef: ElementRef;
   data$: Observable<OverviewPage>;
+  tapTarget: MaterialInstance;
 
   constructor(private analyticsService: AnalyticsService) {}
 
   ngOnInit() {
     this.data$ = this.analyticsService.getOverview();
+  }
+
+  ngAfterViewInit() {
+    this.tapTarget = MaterialService.initTapTarget(this.tapTargetRef);
+  }
+
+  ngOnDestroy() {
+    this.tapTarget.destroy();
+  }
+
+  openInfo() {
+    this.tapTarget.open();
   }
 }
--- client/src/app/shared/services/material.service.ts
+++ client/src/app/shared/services/material.service.ts
@@ -36,4 +36,8 @@ export class MaterialService {
       onClose
     });
   }
+
+  static initTapTarget(ref: ElementRef): MaterialInstance {
+    return M.TapTarget.init(ref.nativeElement);
+  }
 }
