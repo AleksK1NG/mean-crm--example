d917cd5558df3502a2e75430958e1e23b538b68e
Alexander Bryksin <alexander.bryksin@yandex.ru>
2019-06-11 15:14:16 +0300

    add Client Analytics Page Average subscription$

--- client/src/app/analytics-page/analytics-page.component.ts
+++ client/src/app/analytics-page/analytics-page.component.ts
@@ -1,25 +1,35 @@
-import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { AnalyticsService } from '../shared/services/analytics.service';
 import { AnalyticsPage } from '../shared/interfaces/analyticsPage';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-analytics-page',
   templateUrl: './analytics-page.component.html',
   styleUrls: ['./analytics-page.component.css']
 })
-export class AnalyticsPageComponent implements OnInit, AfterViewInit {
+export class AnalyticsPageComponent implements OnInit, AfterViewInit, OnDestroy {
   @ViewChild('gain', null) gainRef: ElementRef;
   @ViewChild('order', null) orderRef: ElementRef;
 
-  average: number
-  loading: true
+  average: number;
+  averageSub$: Subscription;
+  loading = true;
   constructor(private analyticsService: AnalyticsService) {}
 
   ngOnInit() {}
 
   ngAfterViewInit() {
-    this.analyticsService.getAnalytics().subscribe((data: AnalyticsPage) => {
+    this.averageSub$ = this.analyticsService.getAnalytics().subscribe((data: AnalyticsPage) => {
+      this.average = data.average;
       console.log(data);
-    })
+      this.loading = false;
+    });
+  }
+
+  ngOnDestroy() {
+    if (this.averageSub$) {
+      this.averageSub$.unsubscribe();
+    }
   }
 }
